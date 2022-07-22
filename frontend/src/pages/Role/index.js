51e362a7cfa964f6c12b699cff9fa99398c3f34a
kabtamu Degifie <kabtamu.degifie@wku.edu.et>
2022-07-23 01:25:47 +0300

    improve role

--- frontend/src/pages/Role/index.js
+++ frontend/src/pages/Role/index.js
@@ -1,10 +1,7 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { MdAdd } from "react-icons/md";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  fetch as fetchPermissions,
-  reset as pReset,
-} from "../../services/Permission/permission.slice";
+import { fetch as fetchPermissions } from "../../services/Permission/permission.slice";
 import {
   fetch as fetchRoles,
   reset as rReset,
@@ -13,11 +10,6 @@ import {
 function Role() {
   const [selectedRoleId, setSelectedRoleId] = useState("");
   const [selectedRole, setSelectedRole] = useState("");
-
-  // const veiwAllRef = useRef();
-  // const createAllRef = useRef();
-  // const updateAllRef = useRef();
-  // const removeAllRef = useRef();
 
   const dispatch = useDispatch();
   const {
@@ -35,66 +27,92 @@ function Role() {
   const formatPermissions = (perms) => {
     // extract permission name => split name by _ => add to Set => back to Array => return {name, permission}
     return Array.from(
-      new Set(perms?.map((perm) => perm.name.split("_")[1]))
+      new Set(perms.map((perm) => perm.name.split("_")[1]))
     ).map((name) => ({
       name,
-      permissions: perms?.filter((perm) => perm.name.includes(name)),
+      permissions: perms.filter((perm) => perm.name.includes(name)),
     }));
   };
 
   // first time effect
   useEffect(() => {
     dispatch(fetchPermissions());
-    if (roles && roles.length > 0) {
-      setSelectedRoleId(roles[0]._id);
-    }
-    dispatch(pReset());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // dispatcher effect
+  // // dispatcher effect
   useEffect(() => {
     dispatch(fetchRoles());
-    dispatch(rReset());
   }, [dispatch]);
 
   // state effect
   useEffect(() => {
-    if (roles && roles.length > 0) {
-      setSelectedRole(roles.find((role) => role._id === selectedRoleId));
+    if (rSuccess || roles) {
+      if (selectedRoleId) {
+        setSelectedRole(roles.find((role) => role._id === selectedRoleId));
+      } else {
+        setSelectedRoleId(roles[0]._id);
+      }
+      dispatch(rReset());
     }
-    dispatch(rReset());
-  }, [dispatch, roles, selectedRoleId]);
+  }, [dispatch, pSuccess, permissions, rSuccess, roles, selectedRoleId]);
 
   // select role
   const selectRoleHandler = (e) => {
     setSelectedRoleId(e.target.value);
   };
 
-  console.log(rLoading);
+  const checkAllHandler = (e, perms, permType) => {
+    const viewPerm = perms.filter((p) => p.name.includes(permType));
+    if (e.target.checked) {
+      const selectedRolePerms = [...selectedRole.permissions];
+      for (let p of viewPerm) {
+        const index = selectedRolePerms.findIndex((srp) => srp._id === p._id);
+        if (index === -1) {
+          selectedRolePerms.push(p);
+        }
+      }
+      setSelectedRole((prev) => ({
+        name: prev.name,
+        permissions: selectedRolePerms,
+      }));
+    } else {
+      const selectedRolePerms = [...selectedRole.permissions];
+      for (let p of viewPerm) {
+        const index = selectedRolePerms.findIndex((srp) => srp._id === p._id);
+        if (index !== -1) {
+          selectedRolePerms.splice(index, 1);
+        }
+      }
+      setSelectedRole((prev) => ({
+        name: prev.name,
+        permissions: selectedRolePerms,
+      }));
+    }
+  };
+
+  // console.log(selectedRole);z
 
   return (
-    <div className="grid grid-rows-2 gap-2 w-full ml-12 mr-12 border-gray-200 border-2 p-3 rounded-lg">
-      {rLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="border-gray-200 border-2 p-3 rounded-lg">
+    <div className="flex flex-col gap-3  w-full ml-12 mr-12 content-start border-gray-200 border-2 p-3 rounded-lg">
+      <div className="border-gray-200 border-2 p-3 rounded-lg bg-slate-100 h-16">
+        {rLoading || !roles ? (
+          <p>Loading...</p>
+        ) : (
           <div className="flex justify-between">
             <div className="flex justify-start items-center gap-6">
-              <div>
-                <select
-                  onChange={selectRoleHandler}
-                  value={selectedRoleId}
-                  className="w-auto bg-white border-gray-200 border-2 p-2 pr-4 pl-4 rounded-md"
-                >
-                  {roles?.map((role) => (
-                    <option key={role._id} value={role._id}>
-                      {role.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div>{selectedRole?.name}</div>
+              <select
+                onChange={selectRoleHandler}
+                value={selectedRoleId}
+                className="w-auto bg-white border-gray-200 border-2 p-2 pr-4 pl-4 rounded-md"
+              >
+                {roles.map((role) => (
+                  <option key={role._id} value={role._id}>
+                    {role.name}
+                  </option>
+                ))}
+              </select>
+              <div>{selectedRole.name}</div>
             </div>
             <div>
               <button
@@ -106,8 +124,8 @@ function Role() {
               </button>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
 
       <div className="border-gray-200 border-2 p-3 rounded-lg">
         <table className="w-full">
@@ -118,36 +136,18 @@ function Role() {
                 <input
                   type="checkbox"
                   className="h-4 w-4 mr-2 accent-indigo-600"
-                  onChange={(e) => {
-                    if (selectedRole) {
-                      const temp = [...selectedRole.permissions];
-                      const filtered = permissions.filter((cp) =>
-                        cp.name.includes("view")
-                      );
-                      for (let f of filtered) {
-                        const index = temp.findIndex((fp) => fp._id === f._id);
-                        if (index === -1 && e.target.checked) {
-                          temp.push(f);
-                        } else {
-                          temp.splice(index, 1);
-                        }
-                      }
-                      temp.push(...filtered);
-                      const role = {
-                        ...selectedRole,
-                        permissions: temp,
-                      };
-                      setSelectedRole(role);
-                    }
-                  }}
+                  onChange={(e) => checkAllHandler(e, permissions, "view")}
                   id="view"
                 />
                 <label htmlFor="view">View</label>
               </th>
+
               <th className="text-start">
                 <input
                   type="checkbox"
                   className="h-4 w-4 mr-2 accent-indigo-600"
+                  onChange={(e) => checkAllHandler(e, permissions, "create")}
+                  id="create"
                 />
                 <label htmlFor="create">Create</label>
               </th>
@@ -155,6 +155,8 @@ function Role() {
                 <input
                   type="checkbox"
                   className="h-4 w-4 mr-2 accent-indigo-600"
+                  onChange={(e) => checkAllHandler(e, permissions, "update")}
+                  id="update"
                 />
                 <label htmlFor="update">Update</label>
               </th>
@@ -162,16 +164,22 @@ function Role() {
                 <input
                   type="checkbox"
                   className="h-4 w-4 mr-2 accent-indigo-600"
+                  onChange={(e) => checkAllHandler(e, permissions, "remove")}
+                  id="remove"
                 />
                 <label htmlFor="remove">Remove</label>
               </th>
             </tr>
           </thead>
           <tbody>
-            {pLoading ? (
-              <p>Loading...</p>
+            {pLoading || !permissions ? (
+              <tr>
+                <td>
+                  <p>Loading...</p>
+                </td>
+              </tr>
             ) : (
-              formatPermissions(permissions)?.map((perm) => (
+              formatPermissions(permissions).map((perm) => (
                 <tr key={perm.name} className="mb-2 mt-2 text-center">
                   <td>{perm.name}</td>
                   {perm.permissions.map((permission) => (
@@ -179,17 +187,14 @@ function Role() {
                       <input
                         className="h-4 w-4 accent-indigo-600"
                         type="checkbox"
+                        id={permission._id}
+                        name={permission._id}
                         checked={
                           selectedRole?.permissions?.findIndex(
                             (p) => p._id === permission._id
                           ) !== -1
                         }
                         onChange={(e) => {
-                          console.log(
-                            selectedRole?.permissions?.findIndex(
-                              (p) => p._id === permission._id
-                            ) !== -1
-                          );
                           if (selectedRole) {
                             const clonedPermissions = [
                               ...selectedRole.permissions,
@@ -207,7 +212,6 @@ function Role() {
                               ...selectedRole,
                               permissions: clonedPermissions,
                             };
-
                             setSelectedRole(updatedRole);
                           }
                         }}
